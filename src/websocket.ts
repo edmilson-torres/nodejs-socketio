30e5c75aac796720d0b23ef37703c9b6c387ea0b
Edmilson Torres <edmilson.torres@gmail.com>
2022-10-19 23:56:28 -0300

    refactor: add types

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,23 +1,30 @@
 import { io } from './app';
 
-const users: Array<any> = [];
+const users: string[] = [];
+
 const total = (clientsAmount: number) =>
     `\x1b[36mTotal:\x1b[0m ${clientsAmount}`;
 const time = (date: Date) => `[${date.toLocaleTimeString()}]`;
 
+interface Message {
+    room: string;
+    nickname: string;
+    message: string;
+}
+
 io.on('connection', (socket) => {
-    socket.on('room', (data) => {
+    socket.on('room', (data: string) => {
         socket.join(data);
     });
 
-    socket.on('msg', (data) => {
+    socket.on('msg', (data: Message) => {
         io.to(data.room).emit('msg', {
             nickname: data.nickname,
             message: data.message,
         });
     });
 
-    users.push(socket);
+    users.push(socket.id);
     console.info(
         `${time(new Date())} \x1b[32mClient connected\x1b[0m - ${total(
             users.length
@@ -25,7 +32,7 @@ io.on('connection', (socket) => {
     );
 
     socket.on('disconnect', () => {
-        users.splice(users.indexOf(socket), 1);
+        users.splice(users.indexOf(socket.id), 1);
         console.info(
             `${time(new Date())} \x1b[31mClient disconnected\x1b[0m ${total(
                 users.length
